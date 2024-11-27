df7cf8c4a10442799c0508567e8b3082f80c7b43
daniel sacchetti claret <00001100074144sp@al.educacao.sp.gov.br>
2024-11-26 21:13:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'cultura Pop',
+    'qual o simbolo do super heroi Batman?',
+    'Um Morcego'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Matematica',
+    'Peixe + Peixe?',
+    '2 `Peixe'
+)
+
+criaCartao(
+    'Sla',
+    'O Marcio vai me dar o que?',
+    'NOTAAAAA!!!'
+)
